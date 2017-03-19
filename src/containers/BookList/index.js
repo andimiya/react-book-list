ad13c689d6f2d73587f35712eb2fa22369453ca0
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-03-18 20:02:17 -1000

    Saving Progress

--- src/containers/BookList/index.js
+++ src/containers/BookList/index.js
@@ -3,12 +3,33 @@ import BookList from '../../components/BookList.js'
 import './styles.css';
 
 class List extends Component {
-  constructor(){
-    super()
-    this.title = 'Ready Player One'
-    this.author = 'Ernest Cline'
+  constructor(props){
+    super(props)
+
+    this.state = {
+      BookList : []
+    }
+
+    this.allBooks = [
+      {
+        _id: 1,
+        title: 'Ready Player One',
+        author: 'Ernest Cline'
+      },
+      {
+        _id: 2,
+        title: 'Enders Game',
+        author: 'Orson Scott Card'
+      },
+      {
+        _id: 3,
+        title: 'Bladerunner',
+        author: 'Harrison Ford'
+      }
+    ]
   }
-  // 
+
+  //
   // componentWillMount(){
   //   .getBooksFromFakeXHR()
   //     .then( books => {
@@ -18,12 +39,20 @@ class List extends Component {
 
   render() {
     return (
-    <div className="List">
-      <BookList
-        title={this.title}
-        author={this.author}
-      />
-    </div>
+    // <div className="List">
+    //   <BookList
+    //     title={this.title}
+    //     author={this.author}
+    //   />
+    // </div>
+    <ul>
+      this.state.BookList.map( ( { title, author }) =>
+        <BookList
+          title={BookList.title}
+          author={BookList.author}
+        />
+        )
+    </ul>
     )
   }
 }
